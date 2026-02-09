e89951a2cd9cdff074410c1377c70cc98c9d8cb3
Joy-Majumder <gopalmajumdarjoy@gmail.com>
2026-02-10 01:46:45 +0600

    0x1

---
+++ ConditionalExpression.py
@@ -0,0 +1,67 @@
+# IF else and else if in python
+
+# if else and elif statements are a multiway decision taken by our program due to certain conditions in our code.
+
+# a = 12
+# b = 13
+
+# if a>b:
+#     print("a is greater than b")
+# else:
+#     print("a is equal to b") # this is called If else statement
+
+# This is if elif else ladder
+
+# if a>b:
+#     print("a is greater than b")
+
+# elif a<b:
+#     print("a is less than b")
+
+# else:
+#     print("a is equal to b")
+
+# Quiz:
+
+# a = int(input("Enter Your age here : "))
+#
+# if a >= 18:
+#     print("Yes")
+# elif a < 18:
+#     print("No")
+# else:
+#     print("try again next year")
+
+# RELATIONAL OPERATORS
+
+# Relational Operators are used to evaluate conditions inside the if statements. Some examples of relational operators are:
+
+# == : equals.
+# >= : greater than/ equal to.
+# <=: lesser than/ equal to.
+
+# LOGICAL OPERATORS
+#
+# In python logical operators operate on conditional statements. For Example:
+#
+#  and – true if both operands are true else false.
+#  or – true if at least one operand is true or else false.
+#  not – inverts true to false & false to true.
+
+# ELIF CLAUSE
+
+# elif in python means [else if]. An if statements can be chained together with a lot ofthese elif statements followed by an else statement.
+
+#  if (condition1):
+#     #code
+# elif (condition2): # this ladder will stop once a condition in an if or elif is met.
+#     #code
+# elif(condition3):
+#     #code
+# else:
+#     #code
+
+# IMPORTANT NOTES:
+#
+# 1. There can be any number of elif statements.
+# 2. Last else is executed only if all the conditions inside elifs fail.
